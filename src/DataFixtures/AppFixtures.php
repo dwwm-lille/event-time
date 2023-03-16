18727260ff1d4a46bac1e558e2539f0d11f1b1e0
Matthieu Mota <matthieumota@gmail.com>
2023-03-16 15:36:57 +0100

    Entity and fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $event = new Event();
+        $event->setName('Concert 1');
+        $event->setDescription('Lorem');
+        $event->setPrice(10);
+        $event->setStartAt(new \DateTimeImmutable('2023-03-14 11:38:45'));
+        $event->setEndAt(new \DateTimeImmutable('2023-03-15 11:38:45'));
+        $manager->persist($event);
+
+        $event = new Event();
+        $event->setName('Concert 2');
+        $event->setDescription('Lorem');
+        $event->setPrice(20);
+        $event->setStartAt(new \DateTimeImmutable('2023-03-16 10:38:45'));
+        $event->setEndAt(new \DateTimeImmutable('2023-03-18 11:38:45'));
+        $manager->persist($event);
+
+        $event = new Event();
+        $event->setName('Concert 3');
+        $event->setDescription('Lorem');
+        $event->setPrice(30);
+        $event->setStartAt(new \DateTimeImmutable('2023-03-18 11:38:45'));
+        $event->setEndAt(new \DateTimeImmutable('2023-03-19 11:38:45'));
+        $manager->persist($event);
+
+        $manager->flush();
+    }
+}
