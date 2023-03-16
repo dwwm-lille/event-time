e71a2a653b3e88d3289da26be124a3e2e2e7edcd
Matthieu Mota <matthieumota@gmail.com>
2023-03-16 18:56:14 +0100

    Fix fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,7 +13,7 @@ class AppFixtures extends Fixture
         $event = new Event();
         $event->setName('Concert 1');
         $event->setDescription('Lorem');
-        $event->setPrice(10);
+        $event->setPrice(10 * 100);
         $event->setStartAt(new \DateTimeImmutable('2023-03-14 11:38:45'));
         $event->setEndAt(new \DateTimeImmutable('2023-03-15 11:38:45'));
         $manager->persist($event);
@@ -21,7 +21,7 @@ class AppFixtures extends Fixture
         $event = new Event();
         $event->setName('Concert 2');
         $event->setDescription('Lorem');
-        $event->setPrice(20);
+        $event->setPrice(20 * 100);
         $event->setStartAt(new \DateTimeImmutable('2023-03-16 10:38:45'));
         $event->setEndAt(new \DateTimeImmutable('2023-03-18 11:38:45'));
         $manager->persist($event);
@@ -29,7 +29,7 @@ class AppFixtures extends Fixture
         $event = new Event();
         $event->setName('Concert 3');
         $event->setDescription('Lorem');
-        $event->setPrice(30);
+        $event->setPrice(30 * 100);
         $event->setStartAt(new \DateTimeImmutable('2023-03-18 11:38:45'));
         $event->setEndAt(new \DateTimeImmutable('2023-03-19 11:38:45'));
         $manager->persist($event);
